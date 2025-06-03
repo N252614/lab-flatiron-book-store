dcc0a1cc9320c587fe0cfccfd47dc0e8b98e8967
Your Full Name <your-email@example.com>
2025-06-03 12:27:32 -0600

    Flatbook DOM lab finished

--- index.js
+++ index.js
@@ -43,5 +43,22 @@ const bookStore = {
     ]
 }
 
-// Write your code here!
-
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+const placeholder = document.getElementById("delete-this");
+placeholder.remove();
+const bookList = document.getElementById("book-list");
+bookStore.books.forEach((book) => {
+const bookContainer = document.createElement("li");
+const bookTitle = document.createElement("h3");
+bookTitle.textContent = book.title;
+const bookAuthor = document.createElement("p");
+bookAuthor.textContent = "Author: " + book.author;
+const bookImage = document.createElement("img");
+bookImage.src = book.imageUrl;
+bookImage.alt = book.title;
+bookContainer.appendChild(bookTitle);
+bookContainer.appendChild(bookAuthor);
+bookContainer.appendChild(bookImage);
+bookList.appendChild(bookContainer);
+});
